0c77c3f7329d09db1b23802ddae9800256e03bf9
白菜林 <noreply@3ae.cn>
2023-09-11 22:19:24 +0800

    debug

--- _gitpush.sh
+++ _gitpush.sh
@@ -3,6 +3,6 @@
 # git init #
 git add .
 git commit -m "debug"
-git branch -M main #
-git remote add github git@github.com:clin003/php_faka.git #
+# git branch -M main #
+# git remote add github git@github.com:clin003/php_faka.git #
 git push -u github main
